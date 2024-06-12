c6c0a3cccebfd20275c2c6d96e2405d1a26562fa
liujitong <ycljt12@163.com>
2024-06-12 22:41:04 +0800

    文件组织优化调整

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+
+    <img alt="Vue logo" src="./assets/logo.png">
+    <br>
+    <el-radio v-model="radio1" label="1">1</el-radio>
+    <el-radio disabled v-model="radio1" label="2">2</el-radio>
+    <el-radio v-model="radio2" label="3">3</el-radio>
+    <el-radio v-model="radio2" label="4">4</el-radio>
+    <h1 v-if="radio1==1">{{radio1}}</h1>
+    <h2>{{ radio2 }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  methods: {
+  },
+  data () {
+      return {
+        radio1: '1',
+        radio2: '3'
+      };
+    },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
