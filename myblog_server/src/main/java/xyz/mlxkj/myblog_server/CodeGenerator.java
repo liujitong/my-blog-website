f0c8756fad4157724d46fa702d8ccaa5137dbb65
liujitong <ycljt12@163.com>
2024-06-16 11:31:33 +0800

    新增更改

--- myblog_server/src/main/java/xyz/mlxkj/myblog_server/CodeGenerator.java
+++ myblog_server/src/main/java/xyz/mlxkj/myblog_server/CodeGenerator.java
@@ -1,2 +1,42 @@
-package xyz.mlxkj.myblog_server;public class CodeGenerator {
+package xyz.mlxkj.myblog_server;
+
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.nio.file.Paths;
+
+
+/**
+ * mybatisPlus工具
+ */
+// 演示例子，执行 main 方法控制台输入模块表名回车自动生成对应项目目录中
+public class CodeGenerator {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://111.229.170.185:3306/myblog?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Asia/Shanghai", "myblog", "liujitong0A")
+                .globalConfig(builder -> {
+                    builder.author("刘纪彤") // 设置作者
+                            .outputDir("src/main/java"); // 输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("xyz.mlxkj.myblog_server") // 设置父包名
+                            .entity("model") // 设置实体类包名
+                            .mapper("dao") // 设置 Mapper 接口包名
+                            .service("service") // 设置 Service 接口包名
+                            .serviceImpl("service.impl") // 设置 Service 实现类包名
+                            .xml("mappers"); // 设置 Mapper XML 文件包名
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("users") // 设置需要生成的表名
+                            .entityBuilder()
+                            .enableLombok() // 启用 Lombok
+                            .enableTableFieldAnnotation() // 启用字段注解
+                            .controllerBuilder()
+                            .enableRestStyle(); // 启用 REST 风格
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用 Freemarker 模板引擎
+                .execute(); // 执行生成
+    }
+
 }
--- myblog_server/src/main/java/xyz/mlxkj/myblog_server/common/result.java
+++ myblog_server/src/main/java/xyz/mlxkj/myblog_server/common/result.java
@@ -1,2 +1,41 @@
-package xyz.mlxkj.myblog_server.common;public class result {
+package xyz.mlxkj.myblog_server.common;
+
+import lombok.Data;
+import java.io.Serializable;
+
+//封装统一的返回结果
+
+@Data
+public class result implements Serializable {
+    private String Code;
+    private String Msg;
+    private Object Data;
+    public static result succ(Object data) {
+        result m = new result();
+        m.setCode("0");
+        m.setData(data);
+        m.setMsg("操作成功");
+        return m;
+    }
+    public static result succ(String mess, Object data) {
+        result m = new result();
+        m.setCode("0");
+        m.setData(data);
+        m.setMsg(mess);
+        return m;
+    }
+    public static result fail(String mess) {
+        result m = new result();
+        m.setCode("-1");
+        m.setData(null);
+        m.setMsg(mess);
+        return m;
+    }
+    public static result fail(String mess, Object data) {
+        result m = new result();
+        m.setCode("-1");
+        m.setData(data);
+        m.setMsg(mess);
+        return m;
+    }
 }
