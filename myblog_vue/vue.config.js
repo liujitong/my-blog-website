d8a3280ea9b916e6407bf5ee7a52f1d1086d7f7b
liujitong <ycljt12@163.com>
2024-06-22 13:23:43 +0800

    update new

--- myblog_vue/vue.config.js
+++ myblog_vue/vue.config.js
@@ -8,10 +8,11 @@ module.exports = {
     host: 'localhost',
     open: true, // 自动打开浏览器
     // 代理配置表，在这里可以配置特定的请求代理到对应的API接口
-    // 例如将'localhost:8080/api/xxx'代理到'www.example.com/xxx'
+    // 这里是将/api开头的路由代理到yourip:8089的请求
+    //比如 /api/login就是yourip:8089/login
     proxy: {
       '/api': {
-        target: 'http://111.229.170.185:8089',
+        target: 'http://yourip:8089',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
